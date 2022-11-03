fda35bdaeef09fd03bb018bf366d0c42921f1333
JannGarcia <jann.garcia@upr.edu>
2022-11-03 18:07:57 -0400

    Dgts? now displays 1/2

--- settings/Tarea2.Debug.cspy.ps1
+++ settings/Tarea2.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\Vandiel\OneDrive - University of Puerto Rico\5th Year\CIIC4082-100\Tareas\Tarea 2\Code\Tarea-2\settings\Tarea2.Debug.general.xcl" --backend -f "C:\Users\Vandiel\OneDrive - University of Puerto Rico\5th Year\CIIC4082-100\Tareas\Tarea 2\Code\Tarea-2\settings\Tarea2.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "D:\Code Workspaces\IAR Workspaces\Tarea 2\settings\Tarea2.Debug.general.xcl" --backend -f "D:\Code Workspaces\IAR Workspaces\Tarea 2\settings\Tarea2.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\Users\Vandiel\OneDrive - University of Puerto Rico\5th Year\CIIC4082-100\Tareas\Tarea 2\Code\Tarea-2\settings\Tarea2.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Vandiel\OneDrive - University of Puerto Rico\5th Year\CIIC4082-100\Tareas\Tarea 2\Code\Tarea-2\settings\Tarea2.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "D:\Code Workspaces\IAR Workspaces\Tarea 2\settings\Tarea2.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\Code Workspaces\IAR Workspaces\Tarea 2\settings\Tarea2.Debug.driver.xcl" 
 }
